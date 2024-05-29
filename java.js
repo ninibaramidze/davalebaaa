6f30f708a44cdf5987657522e50bab7c69214a76
ninibaramidze <166219192+ninibaramidze@users.noreply.github.com>
2024-05-29 18:48:53 +0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,2 @@
+let sahsualo = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+console.log ((sahsualo[0] + sahsualo[1] + sahsualo[2] + sahsualo[3] + sahsualo[4] + sahsualo[5] + sahsualo[6] + sahsualo[7] + sahsualo[8]) /8)
